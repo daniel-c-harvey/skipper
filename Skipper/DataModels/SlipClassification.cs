7c575c4f7391aa06ba5f30cd72689ad4f6b3985e
daniel-c-harvey <the.real.daniel.harvey@gmail.com>
2025-02-25 09:04:56 -0500

    Skipper scaffolding

---
+++ Skipper/DataModels/SlipClassification.cs
@@ -0,0 +1,16 @@
+﻿using DataBlocks.Migrations;
+
+namespace Skipper.DataModels;
+
+[ScheModel]
+public class SlipClassification
+{
+    public int ClassificationId { get; set; }
+    public string SlipName { get; set; }
+    public decimal MaxLength { get; set; }
+    public decimal MaxBeam { get; set; }
+    public decimal BasePrice { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public DateTime UpdatedTime { get; set; }
+}
---
+++ SkipperCLI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace SkipperCLI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
