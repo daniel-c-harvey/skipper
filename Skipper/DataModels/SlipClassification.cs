10d496e0a01c7203b1aff3fb13dc07873dd052eb
daniel-c-harvey <work.daniel.harvey@gmail.com>
2025-02-27 13:41:28 -0500

    stuff

--- Skipper/DataModels/SlipClassification.cs
+++ Skipper/DataModels/SlipClassification.cs
@@ -7,11 +7,14 @@ public class SlipClassification
 {
     [ScheKey("classification_id")]
     public int ClassificationId { get; set; }
+    [ScheData("slip_name")]
     public string SlipName { get; set; }
+    [ScheData("max_length")]
     public decimal MaxLength { get; set; }
+    [ScheData("max_beam")]
     public decimal MaxBeam { get; set; }
+    [ScheData("base_price")]
     public decimal BasePrice { get; set; }
+    [ScheData("description")]
     public string Description { get; set; }
-    public DateTime CreatedTime { get; set; }
-    public DateTime UpdatedTime { get; set; }
 }
