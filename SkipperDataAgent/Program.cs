7796d49c9411f3da192d98e25accd20f259e6501
daniel-c-harvey <work.daniel.harvey@gmail.com>
2025-07-10 10:05:43 -0400

    Skipper Agent Skelton

---
+++ SkipperDataAgent/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SkipperDataAgent;
+
+
+
+internal class ConsoleUi
+{
+    private class UiOption
+    {
+        public string Prompt { get; init; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
